fb2e875dbec489e24c9cbf7dfee90eb1bacfd335
Angelo <angeloquispesuarez@gmail.com>
2025-11-23 11:24:24 -0500

    Pruebas de conexion con la base de datos y listo pra el front-end

--- public/probar_conexion.php
+++ public/probar_conexion.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../app/config/db.php';
 // Si llegó hasta aquí, la conexión se creó bien
 echo "<h2>Conexión a la base de datos exitosa ✅</h2>";
 
-// Probemos una consulta simple para estar seguros
+// Consulta simple
 $sql = "SELECT NOW() AS fecha_hora_actual";
 $result = $conn->query($sql);
 
@@ -19,8 +19,6 @@ $row = $result->fetch_assoc();
 
 echo "<p>La fecha y hora en MySQL es: <strong>" . $row['fecha_hora_actual'] . "</strong></p>";
 
-// Cerrar el resultado (buena práctica)
+// Cerrar el resultado 
 $result->free();
 
-// Opcional: cerrar la conexión al final (cuando no se use más)
-// $conn->close();
